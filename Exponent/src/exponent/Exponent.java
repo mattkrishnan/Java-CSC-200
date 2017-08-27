be8c3768b2bed2af1f97f3c7c16a4916ed9d3124
Tushar <tbalarajan@gmail.com>
2017-08-27 13:21:50 -0400

    Initial Commit
    Exponent Values

---
+++ Exponent/src/exponent/Exponent.java
@@ -0,0 +1,18 @@
+
+package exponent;
+import java.util.Scanner;
+/**
+ *
+ * @author tb252
+ */
+public class Exponent {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner kybd = new Scanner(System.in);
+        
+    }
+    
+}
